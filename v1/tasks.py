490ada64a4500b73f25bda967bc0fb73f501cc4d
Viresh Soedhwa <vireshsoedhwa@gmail.com>
2021-01-17 23:55:53 -0800

    renamed function

--- v1/tasks.py
+++ v1/tasks.py
@@ -1,121 +1,102 @@
 import requests
 from .models import Thread
 from .models import Post
+import re
 
 from django.core.files.base import ContentFile
 
-def get_all_threads():
+def get_all_b_ylyl_threads():
     x = requests.get('https://a.4cdn.org/b/catalog.json')
 
-        # logger.error(x.json())
-
     for page in x.json():   
-        for thread in page['threads']: 
-            # one_entry = Entry.objects.get(pk=1)
-            
-            try:
-                thethread = Thread.objects.get(threadid=thread['no'])     
+        for thread in page['threads']:             
+            if re.search(r'(yl yl)|(ylyl)', thread['com'], re.IGNORECASE):
+                try:
+                    thethread = Thread.objects.get(threadid=thread['no'])     
 
-                if thethread.active == True:
-                    postlist = requests.get('https://a.4cdn.org/b/thread/' + str(thread['no']) + '.json') 
-                    print("thread : " + str(thread['no']) + " " + str(postlist.status_code))  
-                    
-                    if str(postlist.status_code) != '200':
+                    if thethread.active == True:
+                        postlist = requests.get('https://a.4cdn.org/b/thread/' + str(thread['no']) + '.json') 
+                        posts_online = thread['images']
+                        print("thread : " + str(thread['no']) + "  images in db " + str(thethread.mediacount) + " new ones: " + str(posts_online) + " " + str(postlist.status_code))  
+                                               
+
+                        if str(postlist.status_code) == '404':
+                            print(str(thread['no']) + " inactive")
+                            thethread.active = False
+                            thethread.save()
+                        else:  
+
+                            thethread.mediacount = posts_online
+                            thethread.save()
+
+                            for post in postlist.json()['posts']:
+                                # print("post : " + str(post['no']) + ' -------------------- ')
+                                try:
+                                    newpost = Post(postid=post['no'], thread=thethread)
+                                    newpost.ext = post['ext']
+                                    newpost.tim = post['tim']
+                                    newpost.save()
+                                    # print("ext " + post['ext'])
+                                    # print("tim " + str(post['tim']))
+                                except:
+                                    pass
+
+                except Thread.DoesNotExist:
+                    # print(str(thread['no']) + " DoesNotexist")
+                    newthread = Thread(threadid=thread['no'], summary=thread['com'], mediacount=thread['images'])
+                    newthread.active = True
+                    newthread.save()
+                        
+                    postlist = requests.get('https://a.4cdn.org/b/thread/' + str(thread['no']) + '.json')  
+
+                    if str(postlist.status_code) == '404':
                         print(str(thread['no']) + " inactive")
-                        thethread.active = False
-                        thethread.save()
+                        newthread.active = False
+                        newthread.save()
                     else:            
                         for post in postlist.json()['posts']:
-                            print("post : " + str(post['no']) + ' -------------------- ')
                             try:
                                 newpost = Post(postid=post['no'], thread=newthread)
                                 newpost.ext = post['ext']
                                 newpost.tim = post['tim']
                                 newpost.save()
-                                print("ext " + post['ext'])
-                                print("tim " + str(post['tim']))
                             except:
+                                # IF this is not a media post
                                 pass
 
-            except Thread.DoesNotExist:
-                # print(str(thread['no']) + " DoesNotexist")
-                newthread = Thread(threadid=thread['no'], summary=thread['com'])
-                newthread.active = True
-                newthread.save()
-                    
-                postlist = requests.get('https://a.4cdn.org/b/thread/' + str(thread['no']) + '.json')  
-
-                if str(postlist.status_code) != '200':
-                    print(str(thread['no']) + " inactive")
-                    newthread.active = False
-                    newthread.save()
-                else:            
-                    for post in postlist.json()['posts']:
-                        print("post : " + str(post['no']) + ' -------------------- ')
-                        try:
-                            newpost = Post(postid=post['no'], thread=newthread)
-                            newpost.ext = post['ext']
-                            newpost.tim = post['tim']
-                            newpost.save()
-                            print("ext " + post['ext'])
-                            print("tim " + str(post['tim']))
-                        except:
-                            pass
-
-                # for item in postlist.json():
-                #     print(item)
-
-                    # newpost = Post(postid=)
-                
-                # newpost = Post()
-
-                # print(str(thread['no']) + " Added")
-
-            # newthread = Thread(threadid=thread['no'], summary=thread['com'])
-            # newthread.save()
-            
-
-    # for thread in Thread.objects.all()[:5]:
-        
-    #     try:
-    #         postlist = requests.get('https://a.4cdn.org/b/thread/' + str(thread.threadid) + '.json')  
-
-    #         for post in postlist.json()['posts']:
-    #             print(post['no'])
-
-    #     except:
-    #         print('error')
-    #         thread.delete()
-    #         pass
-
-def filter_threads():
-
-    query = Thread.objects.filter(summary__iregex=r'(yl yl)|(ylyl)')
     
+    query = Thread.objects.all()    
     for thread in query:
         print("===========")
-        print("active " + str(thread.active))
-        print(thread.threadid)
-        print(thread.summary)
-
-        getallposts = Post.objects.filter(thread=thread)
+        print(str(thread.threadid) + " Active : " + str(thread.active) + " count: " + str(thread.mediacount) + " " + thread.summary)
         
-        print("Total posts: " + str(len(getallposts)))
+        # check thread and delete if inactive
 
-        for post in getallposts:
-            print(post.tim)
-            print(post.ext)
-            # media = requests.get('https://i.4cdn.org/b/' + str(post.tim) + str(post.ext))  
+        postlist = requests.get('https://a.4cdn.org/b/thread/' + str(thread.threadid) + '.json') 
 
-            if post.fetched == False:
-                download_media(post, str(post.tim), post.ext)
+        if str(postlist.status_code) == '404':
+            thread.delete()
+        else:        
+            getallposts = Post.objects.filter(thread=thread)    
+            for post in getallposts:
+                if post.fetched == False:
+                    download_media(post, str(post.tim), post.ext)
 
 
 
+def get_all_gif_ylyl_threads():
 
-            # post.mediafile = media.content
-            # post.save()
-            # https://i.4cdn.org/[board]/[4chan image ID].[file extension]
+    return None
+
+# def filter_threads():
+#     query = Thread.objects.all()    
+#     for thread in query:
+#         print("===========")
+#         print(str(thread.threadid) + " Active : " + str(thread.active) + " count: " + str(thread.mediacount) + " " + thread.summary)
+#         getallposts = Post.objects.filter(thread=thread)    
+#         for post in getallposts:
+#             if post.fetched == False:
+#                 download_media(post, str(post.tim), post.ext)
 
 def download_media(post, file, extension):
     
@@ -125,7 +106,27 @@ def download_media(post, file, extension):
         post.mediafile = ContentFile(media.content, name=file+extension)
         post.fetched = True
         post.save()
+        print(post.tim + post.ext + " SAVED")
     else:
-        pass   
+        post.fetched == False
+        post.save()
      
     return
+
+
+# def get_all_ylyl_threads():
+
+#     x = requests.get('https://a.4cdn.org/b/catalog.json')
+#     for page in x.json():   
+#         for thread in page['threads']:     
+
+#             if re.search(r'(yl yl)|(ylyl)', thread['com']):
+                
+#                 print(thread['com'])
+
+#             # try:
+#             #     thethread = Thread.objects.get(threadid=thread['no']) 
+
+
+#             posts_online = thread['images']
+#             print("thread : " + str(thread['no']) + "  images in db new ones: " + str(posts_online))
