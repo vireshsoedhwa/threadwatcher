d5441f2ef100433a86d31806bdc5db2d4aaa1383
Viresh Soedhwa <vireshsoedhwa@gmail.com>
2021-02-14 18:19:26 -0800

    added task to entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -17,6 +17,12 @@ echo "--------------------------------------------------------------------------
 python manage.py qcluster &
 echo "-------------------------------------------------------------------------------------------\n"
 
+>&2 echo "create task"
+echo "from django_q.tasks import schedule; import arrow; schedule('v1.tasks.get_all_ylyl_threads',
+         schedule_type=Schedule.MINUTES,
+         minutes=5,
+         repeats=-1,
+         next_run=arrow.utcnow())"
 
 >&2 echo "Create temporary superuser"
 echo "from django.contrib.auth.models import User; User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com','admin')" | python manage.py shell
